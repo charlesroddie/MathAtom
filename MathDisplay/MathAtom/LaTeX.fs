b2a60035457aa86a18a0e7ed5f7f2e0472935840
Happypig375 <hadrianwttang@outlook.com>
2019-01-28 23:22:48 +0800

    Added cases for braces

--- MathDisplay/MathAtom/LaTeX.fs
+++ MathDisplay/MathAtom/LaTeX.fs
@@ -6,18 +6,27 @@ let toAtom latex =
     let (|Alphabet|NonAlphabet|) c = if ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') then Alphabet else NonAlphabet
     let rec read until chars list =
         match chars with
-        | [] -> list |> Row |> Result.Ok
+        | [] ->
+            List.rev list |> Result.Ok
+        | c::_ when (match until with Until u -> c = u | _ -> false) ->
+            List.rev list |> Result.Ok
         | '\\'::cs ->
             let cmd = 
                 match cs with
                 | (NonAlphabet & c)::_ -> string c
                 | cs -> List.takeWhile (function Alphabet -> true | _ -> false) cs |> System.String.Concat
             match cmd with
-            | "1" -> TestResult___ "It's 1!" |> Result.Ok
+            | "1" -> TestResult___ "It's 1!"::list |> Result.Ok
             //Implement commands
             | _ -> "Unrecognized command: \\" + cmd |> Result.Error
         | '^'::cs ->
-            read OneArgument cs list |> Result.map Superscript
+            read OneArgument cs list |> Result.map (Row >> Superscript >> fun x -> x::list)
         | '_'::cs ->
-            read OneArgument cs list |> Result.map Subscript
-    read All (List.ofSeq latex) [] |> List.rev
+            read OneArgument cs list |> Result.map (fun x -> (Row x |> Subscript)::list)
+        | '{'::cs ->
+            read (Until '}') cs list
+        | '}'::_ ->
+            Result.Error "Missing opening brace"
+
+        
+    read All (List.ofSeq latex) []
